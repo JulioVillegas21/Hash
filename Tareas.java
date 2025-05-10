c104d13c48a9ead6adae6c493766b066077601c3
fran972 <blasonfran@gmail.com>
2025-05-10 17:17:43 -0300

    Add files via upload

--- Tareas.java
+++ Tareas.java
@@ -1,3 +1,65 @@
+import java.util.UUID;
 public class Tareas {
-    
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private String fechaCreacion;
+    private int estado;
+    private boolean eliminado;
+
+    public Tareas(String nombre, String descripcion, String fechaCreacion, int Estado){
+        this.id = UUID.randomUUID().toString();
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaCreacion = fechaCreacion;
+        this.estado = estado;
+        this.eliminado = false;
+    }
+    public void Tareas(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+    public int getEstado() {
+        return estado;
+    }
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    public boolean seteliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+        return eliminado;
+    }
+    public boolean geteliminado(){
+        return eliminado;
+    }
+
+
+
+
+
+
 }
