42bf6e5f3676589ef183a0a180234c567b0a7ff4
fran972 <blasonfran@gmail.com>
2025-05-10 16:31:35 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ public class Main {
      */
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
+        int hash,colision,opcion;
         System.out.println("Ingrese el metodo denasignacion de indice deseado");
         System.out.println("1.Arismetica modular");
         System.out.println("2.Metodo de la multiplicacion");
-        int hash = scanner.nextInt();
+        hash = scanner.nextInt();
         while(hash <1 || hash > 2){
             System.out.println("Ingrese un metodo valido");
             hash = scanner.nextInt();
@@ -17,8 +18,36 @@ public class Main {
         System.out.println("Ingrese el metodo de colision deseado");
         System.out.println("1.Lineal");
         System.out.println("2.Cuadratica");
-        int colision = scanner.nextInt();
-        
+        colision = scanner.nextInt();
+        while(colision <1 || colision > 2){
+            System.out.println("Ingrese un metodo valido");
+            colision = scanner.nextInt();
+        }
+        do{
+            System.out.println("1.Ingresar tarea");
+            System.out.println("2.Editar Tarea");
+            System.out.println("3.Eliminar Tarea");
+            System.out.println("4.Salir");
+            opcion = scanner.nextInt();
+            switch(opcion){
+                case 1:
+                    // Ingresar tarea
+                    break;
+                case 2:
+                    // Editar tarea
+                    break;
+                case 3:
+                    // Eliminar tarea
+                    break;
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+        }while(opcion!=4);
 
 
     }
