e1e58d89e653f5edb3fde2b7a410cf916ec0ee85
fran972 <blasonfran@gmail.com>
2025-05-10 19:14:13 -0300

    Add files via upload

--- Colision.java
+++ Colision.java
@@ -1,3 +1,40 @@
 public class Colision {
     
+
+    public static int ColisionLineal(int indice, Tareas arreglo[] ){//paso como parametro el indice calculado por la funcion hash
+        int x=1;                                                    //y el arreglo para que busque el siguiente espacio vacio
+        while(arreglo[indice] != null){
+            indice = (indice + x);
+            x++;
+            if(indice >= 100){
+                indice = 0; // Si el índice supera el tamaño del arreglo, reiniciamos a 0
+            }
+        }
+        
+        return indice;
+    }
+    /*
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+    
+    public static int ColisionCuadratica(int indice, Tareas arreglo[] ){
+        int x=1;
+        while(arreglo[indice] != null){
+            indice = (indice + x^2);
+            x++;
+            if(indice >= 100){
+                indice = indice%101;
+            }
+
+        }
+        
+        return indice;
+    }
 }
